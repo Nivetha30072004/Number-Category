abe8d337c686e86216d114cade4b9d95f6ceb784
Nivetha30072004 <146367186+Nivetha30072004@users.noreply.github.com>
2024-04-05 07:26:51 +0530

    Add files via upload

---
+++ NumberCategory.java
@@ -0,0 +1,8 @@
+package com.tnsif.day5;
+
+public interface NumberCategory {
+	// Functional interface to check number categories
+		
+		    boolean checkNumberCategory(int number1, int number2);
+		}
+
